e64250977cba33e33420606080de15fd7ee36544
Christopher Obbard <chris.obbard@collabora.com>
2021-02-03 10:55:15 +0000

    ci: Add some substance into the benchmark script
    Signed-off-by: Christopher Obbard <chris.obbard@collabora.com>

--- ci/src/main.rs
+++ ci/src/main.rs
@@ -1,4 +1,25 @@
+use std::path::PathBuf;
+use std::process;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt()]
+enum Opt {
+    #[structopt(help = "process a benchmark file")]
+    ProcessBenchmark {
+        #[structopt(parse(from_os_str), help = "benchmark file")]
+        input: PathBuf,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
-    println!("This code is running on GitHub servers.");
+    match Opt::from_args() {
+        Opt::ProcessBenchmark { input } => {
+            if !input.exists() || !input.is_file() {
+                eprintln!("File {:?} could not be found", input);
+                process::exit(1);
+            }
+            println!("{:?}", input);
+        }
+    }
 }
