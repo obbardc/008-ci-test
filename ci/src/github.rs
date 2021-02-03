3d9af24cac5fb1753654279095d9c774442f8b50
Christopher Obbard <chris.obbard@collabora.com>
2021-02-03 14:43:49 +0000

    attempt to decode the json
    Signed-off-by: Christopher Obbard <chris.obbard@collabora.com>

--- ci/src/github.rs
+++ ci/src/github.rs
@@ -1,4 +1,13 @@
-pub async fn test_github(context: &str) {
+use serde::Deserialize;
+
+#[derive(Debug, Clone, PartialEq, Deserialize)]
+#[non_exhaustive]
+struct ActionContextPayload {
+    event_name: String,
+    //event: EventPayload,
+}
+
+pub async fn test_github(context_str: &str) {
     //let octocrab = octocrab::instance();
     //let issues = octocrab.issues(owner, repo);
 
@@ -8,5 +17,9 @@ pub async fn test_github(context: &str) {
     //};
 
     //print!("{:#?}", issue);
+    let context: ActionContextPayload = serde_json::from_str(&context_str).unwrap();
+
+
+    print!("context_str: {}", context_str);
     print!("context: {:?}", context);
 }
